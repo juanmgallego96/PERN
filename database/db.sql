998c1672fefd38afee2a3792876697031aae5dd2
juanmgallego96 <juanmgallego96@hotmail.com>
2022-11-28 12:00:01 -0500

    Set password db

--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,10 @@
 CREATE DATABASE tasksdb;
+
 CREATE TABLE tasks(
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) UNIQUE,
     description VARCHAR(255)
 );
+
+ALTER USER user_name WITH PASSWORD 'new_password';
+ALTER USER postgres WITH PASSWORD 'postgres';
